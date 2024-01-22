494b1ba1190525473d68c7b14974d76598e114f0
austin1610 <austinsparks09@gmail.com>
2024-01-21 18:32:43 -0600

    Added an outlet and connected WKWebView to the Webview kit, added viewDidLoad() method

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
     class AboutViewController: UIViewController {
+        @IBOutlet var webView: WKWebView!
+        
         @IBAction func close(){
             dismiss(animated: true, completion: nil)
         }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            if let url = Bundle.main.url(
+            forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+         }
+        }
     }
-    
+
 
